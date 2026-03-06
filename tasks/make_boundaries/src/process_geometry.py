95540269df0e4b80c10ccc38b0a8d8c073482eba
Max Kohler <hello@maxkohler.com>
2026-03-06 16:59:25 +0100

    Block out name subs

--- tasks/make_boundaries/src/process_geometry.py
+++ tasks/make_boundaries/src/process_geometry.py
@@ -5,6 +5,12 @@ import pandas as pd
 # before merging them all into one file
 # See: https://wiki.openstreetmap.org/wiki/File:Administrative_Gliederung_Deutschlands_admin_level.png
 
+name_subs = {
+    "Freiburg im Breisgau": "Freiburg",
+    "Dillingen a.d.Donau": "Dillingen an der Donau",
+    "Mühldorf a.Inn": "Mühldorf am Inn",
+}
+
 
 def process_geometry(input_path: str) -> gp.GeoDataFrame:
     fp = "vg250_01-01.utm32s.shape.ebenen/vg250_ebenen_0101"
@@ -14,6 +20,7 @@ def process_geometry(input_path: str) -> gp.GeoDataFrame:
     print("Staat... ", end="")
     country = gp.read_file(f"zip://{input_path}!{fp}/VG250_STA.shp")
     country["admin_level"] = 2
+    country["kind"] = "Staat"
     country_processed = country.loc[country["OBJID"] == "DEBKGVG200000CKM"]
     assert country_processed.shape[0] == 1
     print(f"done ({country_processed.shape[0]} geom)")
@@ -22,6 +29,7 @@ def process_geometry(input_path: str) -> gp.GeoDataFrame:
     print("Land... ", end="")
     laender = gp.read_file(f"zip://{input_path}!{fp}/VG250_LAN.shp")
     laender["admin_level"] = 4
+    country["kind"] = "Land"
     laender["land"] = laender["SN_L"]
     laender_processed = laender.loc[laender["GF"] == 4]
     assert laender_processed.shape[0] == 16
@@ -31,6 +39,7 @@ def process_geometry(input_path: str) -> gp.GeoDataFrame:
     print("Kreis... ", end="")
     kreise = gp.read_file(f"zip://{input_path}!{fp}/VG250_KRS.shp")
     kreise["admin_level"] = 6
+    country["kind"] = "Kreis"
     kreise["land"] = kreise["SN_L"]
     kreise_processed = kreise.loc[kreise["GF"] == 4]
     print(f"done ({kreise_processed.shape[0]} geoms)")
@@ -39,6 +48,7 @@ def process_geometry(input_path: str) -> gp.GeoDataFrame:
     print("Gemeinde... ", end="")
     gemeinden = gp.read_file(f"zip://{input_path}!{fp}/VG250_GEM.shp")
     gemeinden["admin_level"] = 8
+    country["kind"] = "Gemeinde"
     gemeinden["land"] = gemeinden["SN_L"]
     gemeinden_processed = gemeinden.loc[gemeinden["GF"] == 4]
     print(f"done ({gemeinden_processed.shape[0]} geoms)")
@@ -57,5 +67,7 @@ def process_geometry(input_path: str) -> gp.GeoDataFrame:
     res["name"] = res["GEN"]
     res["ars"] = res["ARS"]
     res["id"] = res["OBJID"]
+
+    res["name"] = res["name"].apply(lambda x: name_subs[x] if x in name_subs else x)
 
     return res[output_cols].to_crs("wgs84")
