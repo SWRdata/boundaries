e80015c8eb745073ae35b4e0ed02f26fb9bc58cb
Max Kohler <hello@maxkohler.com>
2026-03-20 10:42:01 +0100

    Load available timestamps from the manifest file

--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -1,27 +1,29 @@
-<script>
-	import { DesignTokens } from '@swr-data-lab/components'
-	import App from '$lib/App.svelte'
-	import siteData from '$lib/site.json'
+<script lang="ts">
+  import { DesignTokens } from "@swr-data-lab/components";
+  import App from "$lib/App.svelte";
+  import siteData from "$lib/site.json";
 
-	import screenshot from '$lib/assets/screenshot.png'
+  import screenshot from "$lib/assets/screenshot.png";
+
+  import { page } from "$app/state";
 </script>
 
 <svelte:head>
-	<title>{siteData.title}</title>
-	<meta name="description" content={siteData.description} />
+  <title>{siteData.title}</title>
+  <meta name="description" content={siteData.description} />
 
-	<meta property="og:url" content="https://www.maxkohler.com/" />
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content={siteData.title} />
-	<meta property="og:description" content={siteData.description} />
-	<meta property="og:image" content={screenshot} />
+  <meta property="og:url" content="https://www.maxkohler.com/" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={siteData.title} />
+  <meta property="og:description" content={siteData.description} />
+  <meta property="og:image" content={screenshot} />
 
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={siteData.title} />
-	<meta name="twitter:description" content={siteData.description} />
-	<meta name="twitter:image" content={screenshot} />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={siteData.title} />
+  <meta name="twitter:description" content={siteData.description} />
+  <meta name="twitter:image" content={screenshot} />
 </svelte:head>
 
 <DesignTokens theme="light">
-	<App />
+  <App timestamps={page.data.timestamps} />
 </DesignTokens>
