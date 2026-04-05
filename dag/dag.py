fcdb163c06211932a3dbaa7eff614b2fe6b69c0f
Max Kohler <hello@maxkohler.com>
2026-04-05 15:57:03 +0200

    chore: Drop unused imports, variables, suppress ty import warnings

--- dag/dag.py
+++ dag/dag.py
@@ -1,25 +1,20 @@
-from datetime import timedelta
-import yaml
+# ty: ignore[unresolved-import]
 
+from datetime import timedelta
+
+from airflow import DAG
 from airflow.models.baseoperator import chain
 from airflow.operators.dummy import DummyOperator
-from airflow.utils.dates import days_ago
-from airflow import DAG
-
-from kubernetes.client import models as k8s
+from airflow.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.operators.pod import (
     KubernetesPodOperator,
 )
-
-
-from airflow.operators.python import PythonOperator
-
+from airflow.utils.dates import days_ago
+from airflow_basics.config_loader import load_config
 from airflow_basics.datacatalog_update_bq import update_bq_entry
 from airflow_basics.datacatalog_update_dataplex import update_dataplex
-from airflow_basics.failure_reporting import failure_reporter, check_failures
-from airflow_basics.config_loader import load_config
-
-
+from airflow_basics.failure_reporting import check_failures, failure_reporter
+from kubernetes.client import models as k8s
 
 dag_name = "boundaries"
 
@@ -47,23 +42,16 @@ with DAG(
     is_paused_upon_creation=dag_metadata["is_paused_upon_creation"],
     start_date=days_ago(dag_metadata["start_date"]),
 ) as dag:
-    
     # dummy operators for flexibility
-    pipeline_start = DummyOperator(
-        task_id="pipeline_start"
-    )
-    pipeline_end = DummyOperator(
-        task_id="pipeline_end",
-        trigger_rule="all_done"
-    )
+    pipeline_start = DummyOperator(task_id="pipeline_start")
+    pipeline_end = DummyOperator(task_id="pipeline_end", trigger_rule="all_done")
     failure_report_task = PythonOperator(
         task_id="failure_report",
         python_callable=failure_reporter,
         trigger_rule="all_done",
-        op_kwargs={'disable_reporting': True},
+        op_kwargs={"disable_reporting": True},
         provide_context=True,
     )
-    trigger_task_list = []
     task_list = []
     for task in dag_tasks:
         current_task = KubernetesPodOperator(
@@ -83,7 +71,7 @@ with DAG(
             ],
         )
         task_list.append(current_task)
-    
+
     # tasks are run sequentially depending on the order they are defined in configuration.yaml
     chain(*task_list)
     if dataplex_metadata.get("dataset_id"):
@@ -113,10 +101,6 @@ with DAG(
         print(
             "No dataset_id provided in the configuration, skipping update_dataplex and update_bq"
         )
-    
-    (
-        pipeline_start
-        >> task_list
-        >> pipeline_end
-    )
+
+    (pipeline_start >> task_list >> pipeline_end)
     pipeline_end >> failure_report_task
