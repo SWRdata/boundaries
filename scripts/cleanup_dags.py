47e8d6fef8de33c3c3deed0b8b7b154a24d8732d
Max Kohler <hello@maxkohler.com>
2026-07-26 10:59:59 +0200

    refactor

---
+++ scripts/cleanup_dags.py
@@ -0,0 +1,26 @@
+"""
+Clean up unused DAG files in the remote storage
+"""
+
+import glob
+import os
+import re
+import secrets
+import subprocess
+from sys import stdout
+
+from tap import Tap
+
+
+class ArgumentParser(Tap):
+    base_dir: str = "tasks"  # base directory to search for dockerfiles
+    registry_base: str  # artifact registry base url
+    bucket_name: str = ""  # name of your Managed Airflow's DAGs bucket
+
+
+def main(args: ArgumentParser):
+    pass
+
+
+if __name__ == "__main__":
+    main(ArgumentParser(description=__doc__).parse_args())
